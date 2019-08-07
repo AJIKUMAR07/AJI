147f8308ee82bf242583ef5c396bff0cc6b624b2
AJIKUMAR07 <52313919+AJIKUMAR07@users.noreply.github.com>
2019-08-07 15:09:41 +0530

    Update PRO.5.py

--- PRO.5.py
+++ PRO.5.py
@@ -1,7 +1,7 @@
-r, t, u= map(int,input().split())
-if r == 224:
+p_3, t_3, r_3= map(int,input().split())
+if p_3 == 224:
   print("YES")
-elif(r%t+u) == 0):
+elif(p_3%(t_3+r_3) == 0):
   print("YES")
 else:
   print("NO")
